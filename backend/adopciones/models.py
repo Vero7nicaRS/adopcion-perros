5136059fa04fe4fee3a2dd702b3a8eb954f96f4d
Verónica <vero7nicaxs@gmail.com>
2026-07-27 13:57:05 +0200

    models: Actualizar "__str__" de Temperament

--- backend/adopciones/models.py
+++ backend/adopciones/models.py
@@ -30,7 +30,7 @@ class Temperament(models.Model):
     name = models.CharField(max_length=50 , unique=True)
     
     def __str__(self):
-        return self.name
+        return f"{self.name} - ID {self.id}"
 
 # --------------------------
 #       LOCATION
