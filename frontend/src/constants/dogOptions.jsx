236f1edcaddcf83c3e9d5c16976c808f0f8a31ee
Verónica <vero7nicaxs@gmail.com>
2026-09-06 21:16:58 +0200

    dogOptions: se añade opciones para el estado de la solicitud y la edad del animal

--- frontend/src/constants/dogOptions.jsx
+++ frontend/src/constants/dogOptions.jsx
@@ -20,8 +20,26 @@ export const COMPATIBLE_OPTIONS = [
     { value: "UNKNOWN", label: "Desconocido" }
 ];
 
-
 export const ADOPTION_STATUS_OPTIONS = [
     { value: "AVAILABLE", label: "Disponible" },
     { value: "UNAVAILABLE", label: "No disponible" }
 ];
+
+export const APPLICATIONS_STATUS_OPTIONS = [
+    { value: "PENDING", label: "Pendiente" },
+    { value: "ACCEPTED", label: "Aceptada" },
+    { value: "REJECTED", label: "Rechazada" }  
+]
+/*
+    Cachorro --> menos de 1 año
+    Joven    --> 1-2 años
+    Adulto   --> 3-7 años
+    Senior   --> 8 años o más
+*/
+export const AGE_OPTIONS = [
+    {value: "UNDER_1" , label: "Menos de 1 año"},
+    {value: "1_3", label: "1-3 Años"},
+    {value: "4_7", label: "4-7 Años"},
+    {value: "8_PLUS", label: "Senior"}
+
+]
